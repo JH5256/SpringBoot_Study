0a6b9f43993b578b209d53b15366d558aa769a1f
JH5256 <skdltmvhs2@naver.com>
2024-04-02 11:14:20 +0900

    fifth commit

---
+++ chap06-Interceptor/src/main/java/com/ohgiraffers/interceptor/WebComfiguration.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebComfiguration implements WebMvcConfigurer {
+
+    private final StopWatchInterceptor stopWatchInterceptor;
+
+    /* 생성자 의존성 주입 */
+    public WebComfiguration(StopWatchInterceptor stopWatchInterceptor) {
+        this.stopWatchInterceptor = stopWatchInterceptor;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(stopWatchInterceptor).addPathPatterns("/stopwatch");
+    }
+}
